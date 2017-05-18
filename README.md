d72987bc03b7350e2d39e01b899e99629412ce7f
bthan <bthan@us.ibm.com>
2017-05-17 19:03:51 -0500

    some progress

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
 # todolist-lab
 todolist app for General Assembly Austin class on cloud native
+
+## Install
+
+```sh
+npm init -y
+npm i carbon-components -S
+npm i node-sass-chokidar npm-run-all postcss-cli autoprefixer -D
+```
+
+## src and dist folders
+```
+├── dist
+│   ├── index.html
+│   └── script.js
+├── package.json
+└── src
+    └── style.scss
+
+```
+
+## npm scripts for sass
+
+> **CSS only**: You can also use CSS via CDN [here](https://unpkg.com/carbon-components@7.4.3/css/carbon-components.css)
+
+**ui-application/package.json:**
+```json
+"scripts": {
+  "scss:build": "node-sass-chokidar src/ -o dist/",
+  "scss:watch": "node-sass-chokidar src/ -o dist/ --watch --recursive",
+  "autoprefixer": "postcss dist/style.css --use autoprefixer -o dist/style.css",
+  "scss": "npm-run-all autoprefixer scss:*"
+},
+```
+
+- `npm run scss:build`: use `node-sass-chokidar` to compile any `.scss` files in **src** and output it to **dist**
+- `npm run scss:watch`: do initial build and watch for changes
+- `npm run autoprefixer`: add vendor prefixes to style.css
+- `npm run scss`: main script for dev, runs all above scripts above. Keep this running while you work in scss files.
+
+Run bff-application and `npm run scss` in two different terminal windows:
+
+## Planning your components
+
+![original-design](https://cloud.githubusercontent.com/assets/4185382/26179684/b68c53d4-3b2a-11e7-95a9-6655cfbffc46.png)
+
+What components are we working with?
+
+- title
+- text-field
+- todo-item
+  - checkbox
+  - todo-item-text
+  - delete-button
+
+![deconstructed](https://cloud.githubusercontent.com/assets/4185382/26179983/3d25c212-3b2c-11e7-8c93-632026e9170d.png)
+
+What can we use from `carbon-components`? 
+- checkbox
+- button
+- icons
+- text input
+- h1
+
+## HTML
+
+**index.html:**
+```html
+<body>
+  <div class="container">
+    <h2>Todo List</h2>
+    <div>
+      <div class="formInputArea">
+        <input id="inputText" class="inputText" type="text" placeholder="What needs to be done?" autofocus="">
+      </div>
+      <ul id="todoList" class="todoList">
+      </ul>
+    </div>
+  </div>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="./script.js"></script>
+</body>
+```
+
+**index.html w/carbon-components:** 
---
+++ ui-application/dist/script.js
@@ -0,0 +1,107 @@
+var todos = [];
+//this base url should point to where the BFF Server is running
+var baseurl = 'http://localhost:8080';
+
+//when a mouse enters the list items, show the x delete button
+$(document).ready(function() {
+  $(document)
+    .on('mouseenter', '.items', function() {
+      $(this).find(':button').show();
+    })
+    .on('mouseleave', '.items', function() {
+      $(this).find(':button').hide();
+    });
+});
+
+// check for keydown inside the input text field.
+$('#inputText').keydown(function(e) {
+  var item = $('#inputText').val();
+  //if the keydown was enter and there is an item in the field
+  if (e.which == 13 && item.length > 0) {
+    //set the input text back to blank and make POST request
+    $('#inputText').val('');
+    $.ajax({
+      method: 'POST',
+      url: baseurl + '/todoitems',
+      contentType: 'application/json',
+      data: JSON.stringify({ todo: item }),
+    }).done(function(data) {
+      //reload the items
+      loadItems();
+    });
+  }
+});
+//load the list items from the bff api
+function loadItems() {
+  $.get(baseurl + '/todoitems').done(function(data) {
+    if (data.length > 0) {
+      todos = [];
+      for (var i = 0; i < data.length; i++) {
+        todos.push(data[i]);
+      }
+      refreshList();
+    }
+  });
+}
+//on page load, call load items
+loadItems();
+
+const todoItem = () => {
+  return `
+    <input id="bx--checkbox-1" class="bx--checkbox" type="checkbox" value="green" name="checkbox">
+      <label for="bx--checkbox-1" class="bx--checkbox-label">
+        <span class="bx--checkbox-appearance">
+          <svg class="bx--checkbox-checkmark">
+            <svg width="12" height="9" viewBox="0 0 12 9" fill-rule="evenodd">
+              <path d="M4.1 6.1L1.4 3.4 0 4.9 4.1 9l7.6-7.6L10.3 0z"></path>
+            </svg>
+          </svg>
+        </span>
+      </label>
+  `;
+};
+
+const oldTodoItem = () => {
+  return (
+    '<li class="items"><input class="toggle" type="checkbox">' +
+    todos[i].todo +
+    '<button class="button" onclick="deleteItem(' +
+    i +
+    ')">X</button></li>'
+  );
+};
+//refresh the list by first emptying it, then appending items from todo array
+function refreshList() {
+  $('#todoList').empty();
+  for (var i = 0; i < todos.length; i++) {
+    $('#todoList').append(
+      `<li class="item">
+        <input id="bx--checkbox-1" class="bx--checkbox" type="checkbox" value="green" name="checkbox">
+        <label for="bx--checkbox-1" class="bx--checkbox-label">
+          <span class="bx--checkbox-appearance">
+            <svg class="bx--checkbox-checkmark" idth="12" height="9" viewBox="0 0 12 9" fill-rule="evenodd">
+              <path d="M4.1 6.1L1.4 3.4 0 4.9 4.1 9l7.6-7.6L10.3 0z"></path>
+            </svg>
+          </span>
+        </label>
+        ${todos[i].todo}
+        <svg class="button" onclick="deleteItem(${i}) width="16" height="16" viewBox="0 0 16 16" fill-rule="evenodd">
+          <path d="M8 0C3.6 0 0 3.6 0 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8zm3.5 10.1l-1.4 1.4L8 9.4l-2.1 2.1-1.4-1.4L6.6 8 4.5 5.9l1.4-1.4L8 6.6l2.1-2.1 1.4 1.4L9.4 8l2.1 2.1z"></path>
+        </svg>
+      </li>
+      `
+    );
+  }
+}
+
+//delete items from the todos array and from db via DELETE request
+function deleteItem(index) {
+  var id = todos[index]._id;
+  $.ajax({
+    method: 'DELETE',
+    url: baseurl + '/todoitem/' + id,
+    contentType: 'application/json',
+  });
+  todos.splice(index, 1);
+  refreshList();
+}
